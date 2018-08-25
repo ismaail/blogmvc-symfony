87ecaea04c48035087416179f88151ee157a4090
ismaail <contact@ismaail.com>
2018-08-25 02:08:14 +0100

    Update PostRepositoryTest

--- tests/Repository/PostRepositoryTest.php
+++ tests/Repository/PostRepositoryTest.php
@@ -37,6 +37,91 @@ class PostRepositoryTest extends DatabaseTestCase
     /**
      * @test
      */
+    public function it_returns_paginated_posts()
+    {
+        // Preparations
+        $this->createPost();
+        $this->createPost();
+        $this->entityManager->flush();
+
+        // Actions
+        $paginator = $this->postRepository->paginate(1, 10);
+
+        // Assertions
+        $this->assertEquals(2, $paginator->count(), 'Wrong Post Paginator count.');
+    }
+
+    /**
+     * @test
+     */
+    public function it_returns_paginated_post_by_category()
+    {
+        // Preparations
+        $category1 = $this->createCategory(['name' => 'Cat 1']);
+        $category2 = $this->createCategory(['name' => 'Cat 2']);
+
+        $this->createPost([], $category1);
+        $this->createPost([], $category1);
+        $this->createPost([], $category2);
+
+        $this->entityManager->flush();
+
+        // Actions
+        $paginator1 = $this->postRepository->paginate(1, 10, ['category' => 'cat-1']);
+        $paginator2 = $this->postRepository->paginate(1, 10, ['category' => 'cat-2']);
+
+        // Assertions
+        $this->assertEquals(2, $paginator1->count(), 'Wrong Post Paginator count.');
+        $this->assertEquals(1, $paginator2->count(), 'Wrong Post Paginator count.');
+
+        foreach ($paginator1 as $post) {
+            /** @var \App\Entity\Post $post */
+            $this->assertEquals('Cat 1', $post->getCategory()->getName(), 'Wrong Post Category Name.');
+        }
+
+        foreach ($paginator2 as $post) {
+            /** @var \App\Entity\Post $post */
+            $this->assertEquals('Cat 2', $post->getCategory()->getName(), 'Wrong Post Category Name.');
+        }
+    }
+
+    /**
+     * @test
+     */
+    public function it_returns_paginated_post_by_author()
+    {
+        // Preparations
+        $author1 = $this->createUser(['username' => 'user_A']);
+        $author2 = $this->createUser(['username' => 'user_B']);
+
+        $this->createPost([], null, $author1);
+        $this->createPost([], null, $author2);
+        $this->createPost([], null, $author2);
+
+        $this->entityManager->flush();
+
+        // Actions
+        $paginator1 = $this->postRepository->paginate(1, 10, ['author' => 'user_A']);
+        $paginator2 = $this->postRepository->paginate(1, 10, ['author' => 'user_B']);
+
+        // Assertions
+        $this->assertEquals(1, $paginator1->count(), 'Wrong Post Paginator count.');
+        $this->assertEquals(2, $paginator2->count(), 'Wrong Post Paginator count.');
+
+        foreach ($paginator1 as $post) {
+            /** @var \App\Entity\Post $post */
+            $this->assertEquals('user_A', $post->getAuthor()->getUsername(), 'Wrong Post Author username.');
+        }
+
+        foreach ($paginator2 as $post) {
+            /** @var \App\Entity\Post $post */
+            $this->assertEquals('user_B', $post->getAuthor()->getUsername(), 'Wrong Post Author username.');
+        }
+    }
+
+    /**
+     * @test
+     */
     public function it_returns_a_single_post_by_slug()
     {
         $this->createPost(['title' => 'Some Title 123']);
