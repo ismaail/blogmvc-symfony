acbaa76b748d47df016b6f777208eee569cf059f
ismaail <contact@ismaail.com>
2018-10-28 18:13:41 +0000

    Add comments property to Post Entity

--- src/Entity/Post.php
+++ src/Entity/Post.php
@@ -4,6 +4,8 @@ namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use Gedmo\Mapping\Annotation as Gedmo;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\Common\Collections\ArrayCollection;
 
 /**
  * Class Post
@@ -53,6 +55,19 @@ class Post
      * @ORM\JoinColumn(nullable=false)
      */
     private $author;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Comment", mappedBy="post", orphanRemoval=true)
+     */
+    private $comments;
+
+    /**
+     * Post constructor.
+     */
+    public function __construct()
+    {
+        $this->comments = new ArrayCollection();
+    }
 
     /**
      * @return int|null
@@ -178,6 +193,47 @@ class Post
     public function setAuthor(?User $author): Post
     {
         $this->author = $author;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Comment[]
+     */
+    public function getComments(): Collection
+    {
+        return $this->comments;
+    }
+
+    /**
+     * @param \App\Entity\Comment $comment
+     *
+     * @return \App\Entity\Post
+     */
+    public function addComment(Comment $comment): Post
+    {
+        if (! $this->comments->contains($comment)) {
+            $this->comments[] = $comment;
+            $comment->setPost($this);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @param \App\Entity\Comment $comment
+     *
+     * @return \App\Entity\Post
+     */
+    public function removeComment(Comment $comment): Post
+    {
+        if ($this->comments->contains($comment)) {
+            $this->comments->removeElement($comment);
+            // set the owning side to null (unless already changed)
+            if ($comment->getPost() === $this) {
+                $comment->setPost(null);
+            }
+        }
 
         return $this;
     }
