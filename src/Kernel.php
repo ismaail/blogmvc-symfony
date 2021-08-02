7de002114aba0791f369fb259590c18c1ab993f2
ismaail <contact@ismaail.com>
2021-08-02 05:09:49 +0100

    Updated Application files: kernel.php, public/index.php & bin/console

--- src/Kernel.php
+++ src/Kernel.php
@@ -23,13 +23,11 @@ class Kernel extends BaseKernel
         $container->import('../config/{packages}/*.yaml');
         $container->import('../config/{packages}/' . $this->environment . '/*.yaml');
 
-        $path = \dirname(__DIR__) . '/config/services.php';
-
         if (is_file(\dirname(__DIR__) . '/config/services.yaml')) {
             $container->import('../config/services.yaml');
             $container->import('../config/{services}_' . $this->environment . '.yaml');
-        } elseif (is_file($path)) {
-            (require $path)($container->withPath($path), $this);
+        } else {
+            $container->import('../config/{services}.php');
         }
     }
 
@@ -41,12 +39,10 @@ class Kernel extends BaseKernel
         $routes->import('../config/{routes}/' . $this->environment . '/*.yaml');
         $routes->import('../config/{routes}/*.yaml');
 
-        $path = \dirname(__DIR__) . '/config/routes.php';
-
         if (is_file(\dirname(__DIR__) . '/config/routes.yaml')) {
             $routes->import('../config/routes.yaml');
-        } elseif (is_file($path)) {
-            (require $path)($routes->withPath($path), $this);
+        } else {
+            $routes->import('../config/{routes}.php');
         }
     }
 }
